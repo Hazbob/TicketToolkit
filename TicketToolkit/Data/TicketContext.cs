81c91fe6a5176509ae6ef2f73b3bc3b9e2ec9bb0
hazbob <hrobinson3781@gmail.com>
2024-07-23 21:55:05 +0100

    database setup

---
+++ TicketToolkit/Data/TicketContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using TicketToolkit.Models;
+
+namespace TicketToolkit.Data;
+
+public class TicketContext(DbContextOptions<TicketContext> options) : DbContext(options)
+{
+    public DbSet<Ticket> Tickets { get; set; }
+}
